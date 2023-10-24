c3564f0f1cb15d1a51eba75a624fce2bac86f82f
c483481 <charleskojansow@gmail.com>
2023-10-25 03:00:29 +0800

    feat: add endpoint login

--- src/server/controller/auth.controller.ts
+++ src/server/controller/auth.controller.ts
@@ -3,7 +3,7 @@ import { AppServiceMap, AuthService } from "../../contract/service.contract";
 import { BaseController } from "./base.controller";
 import { WrapAppHandler } from "../../handler/default.handler";
 import { validate } from "../validate";
-import { Register_Payload } from "../dto/auth.dto";
+import { Login_Payload, Register_Payload } from "../dto/auth.dto";
 import { AuthValidator } from "../validate/auth.validator";
 import { defaultMiddleware } from "../../utils/middleware-helper.utils";
 import { Privilege } from "../../constant/privilege.constant";
@@ -21,6 +21,8 @@ export class AuthController extends BaseController {
 
     initRoute(): void {
         this.router.post("/register", defaultMiddleware(Privilege.Admin), WrapAppHandler(this.postRegister));
+
+        this.router.post("/login", WrapAppHandler(this.postLogin));
     }
 
     postRegister = async (req: Request): Promise<unknown> => {
@@ -29,6 +31,14 @@ export class AuthController extends BaseController {
         validate(AuthValidator.Register_Payload, payload);
 
         const result = await this.service.register(payload);
+
+        return result;
+    };
+
+    postLogin = async (req: Request): Promise<unknown> => {
+        const payload = req.body as Login_Payload;
+
+        const result = await this.service.login(payload);
 
         return result;
     };
--- src/server/validate/auth.validator.ts
+++ src/server/validate/auth.validator.ts
@@ -10,4 +10,10 @@ export class AuthValidator {
         },
         $$strict: true,
     });
+
+    static Login_Payload = baseValidator.compile({
+        username: "string|empty:false|required|min:5|max:255",
+        password: "string|empty:false|required|min:5|max:255",
+        $$strict: true,
+    });
 }
