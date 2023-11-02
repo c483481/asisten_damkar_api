61f44d45b4d96f34d9453cd94782a1840a12d859
c483481 <charleskojansow@gmail.com>
2023-11-02 23:33:15 +0800

    add create truck on truck service

--- src/server/service/truck.service.ts
+++ src/server/service/truck.service.ts
@@ -1,12 +1,13 @@
 import { isValid } from "ulidx";
 import { AppRepositoryMap, PosRepository, TruckRepository } from "../../contract/repository.contract";
-import { createData } from "../../utils/helper.utils";
-import { TruckCreation_Payload } from "../dto/truck.dto";
-import { TruckCreationsAttributes } from "../model/truck.model";
+import { composeResult, createData } from "../../utils/helper.utils";
+import { TruckCreation_Payload, TruckResult } from "../dto/truck.dto";
+import { TruckAttribute, TruckCreationsAttributes } from "../model/truck.model";
 import { BaseService } from "./base.service";
 import { errorResponses } from "../../response";
+import { TruckService } from "../../contract/service.contract";
 
-export class Truck extends BaseService {
+export class Truck extends BaseService implements TruckService {
     private truckRepo!: TruckRepository;
     private posRepo!: PosRepository;
     init(repository: AppRepositoryMap): void {
@@ -14,20 +15,35 @@ export class Truck extends BaseService {
         this.posRepo = repository.pos;
     }
 
-    createTruck = async (payload: TruckCreation_Payload) => {
+    createTruck = async(payload: TruckCreation_Payload) => {
         const { plat, posXid, userSession } = payload;
 
-        if (!isValid(posXid)) {
-            throw errorResponses.getError("E_REQ_1");
+        if(!isValid(posXid)) {
+            throw errorResponses.getError("E_FOUND_1");
         }
 
-        const createdValues = createData<TruckCreationsAttributes>(
-            {
-                plat,
-                posXid,
-                active: true,
-            },
-            userSession
-        );
-    };
+        const pos = await this.posRepo.findByXid(posXid);
+
+        if(!pos) {
+            throw errorResponses.getError("E_FOUND_1");
+        }
+
+        const createdValues = createData<TruckCreationsAttributes>({
+            plat,
+            posXid,
+            active: true,
+        }, userSession);
+
+        const result = await this.truckRepo.insertTruck(createdValues);
+
+        return composeTruck(result);
+    }
+}
+
+export function composeTruck(row: TruckAttribute): TruckResult {
+    return composeResult<TruckAttribute, TruckResult>(row, {
+        posXid: row.posXid,
+        plat: row.plat,
+        active: row.active,
+    })
 }
